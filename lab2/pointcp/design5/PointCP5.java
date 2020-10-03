e7c1a3048507f3223fc7f237cf9be3eaf9dc0386
Wilson Huang <71933130+wilsonhuang326@users.noreply.github.com>
2020-10-03 11:21:14 -0400

    moving design5

--- lab2/pointcp/design5/PointCP5.java
+++ lab2/pointcp/design5/PointCP5.java
@@ -1,4 +1,5 @@
-package design5;// This file contains material supporting section 2.9 of the textbook:
+package design5;
+// This file contains material supporting section 2.9 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
 // license found at www.lloseng.com
 /**
