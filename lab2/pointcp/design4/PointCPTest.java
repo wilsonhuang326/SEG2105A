19c5899eecae4a841bf382ac7e55fa3a815ccaaf
Wilson Huang <71933130+wilsonhuang326@users.noreply.github.com>
2020-10-04 15:24:28 -0400

    design4

--- lab2/pointcp/design4/PointCPTest.java
+++ lab2/pointcp/design4/PointCPTest.java
@@ -1,11 +1,12 @@
+package design4;
 // This file contains material supporting section 2.9 of the textbook:
 // "Object Oriented Software Engineering" and is issued under the open-source
-// license found at www.lloseng.com 
+// license found at www.lloseng.com
 
 import java.io.*;
 
 /**
- * This class prompts the user for a set of coordinates, and then 
+ * This class prompts the user for a set of coordinates, and then
  * converts them from polar to cartesian or vice-versa.
  *
  * @author Fran&ccedil;ois B&eacute;langer
@@ -13,14 +14,13 @@ import java.io.*;
  * @author Paul Holden
  * @version July 2000
  */
-public class PointCPTest
-{
+public class PointCPTest {
   //Class methods *****************************************************
 
   /**
    * This method is responsible for the creation of the PointCP
    * object.  This can be done in two ways; the first, by using the
-   * command line and running the program using <code> java 
+   * command line and running the program using <code> java
    * PointCPTest &lt;coordtype (c/p)&gt; &lt;X/RHO&gt; &lt;Y/THETA&gt;
    * </code> and the second by getting the program to prompt the user.
    * If the user does not enter a valid sequence at the command line,
@@ -31,43 +31,32 @@ public class PointCPTest
    * @param args[1] The value of X or RHO.
    * @param args[2] The value of Y or THETA.
    */
-  public static void main(String[] args)
-  {
-    PointCP point;
+  public static void main(String[] args) {
+    PointCP4 point;
 
-    System.out.println("Cartesian-Polar Coordinates Conversion Program");
+    System.out.println("Store both Polar and Cartesian Coordinates Program");
 
     // Check if the user input coordinates from the command line
     // If he did, create the PointCP object from these arguments.
     // If he did not, prompt the user for them.
-    try
-    {
-      point = new PointCP(args[0].toUpperCase().charAt(0), 
-        Double.valueOf(args[1]).doubleValue(), 
-        Double.valueOf(args[2]).doubleValue());
-    }
-    catch(Exception e)
-    {
+    try {
+      point = new PointCP4(args[0].toUpperCase().charAt(0),
+              Double.valueOf(args[1]).doubleValue(),
+              Double.valueOf(args[2]).doubleValue());
+    } catch (Exception e) {
       // If we arrive here, it is because either there were no
       // command line arguments, or they were invalid
-      if(args.length != 0)
+      if (args.length != 0)
         System.out.println("Invalid arguments on command line");
 
-      try
-      {
+      try {
         point = getInput();
-      }
-      catch(IOException ex)
-      {
+      } catch (IOException ex) {
         System.out.println("Error getting input. Ending program.");
         return;
       }
     }
     System.out.println("\nYou entered:\n" + point);
-    point.convertStorageToCartesian();
-    System.out.println("\nAfter asking to store as Cartesian:\n" + point);
-    point.convertStorageToPolar();
-    System.out.println("\nAfter asking to store as Polar:\n" + point);
   }
 
   /**
@@ -75,17 +64,17 @@ public class PointCPTest
    * it is valid.  When the input is valid, it returns a PointCP
    * object.
    *
-   * @return A PointCP constructed using information obtained 
-   *         from the user.
+   * @return A PointCP constructed using information obtained
+   * from the user.
    * @throws IOException If there is an error getting input from
-   *         the user.
+   *                     the user.
    */
-  private static PointCP getInput() throws IOException
+  private static PointCP4 getInput() throws IOException
   {
     byte[] buffer = new byte[1024];  //Buffer to hold byte input
     boolean isOK = false;  // Flag set if input correct
     String theInput = "";  // Input information
-    
+
     //Information to be passed to the constructor
     char coordType = 'A'; // Temporary default, to be set to P or C
     double a = 0.0;
@@ -97,38 +86,38 @@ public class PointCPTest
       while (!(isOK))
       {
         isOK = true;  //flag set to true assuming input will be valid
-          
+
         // Prompt the user
         if (i == 0) // First argument - type of coordinates
         {
           System.out.print("Enter the type of Coordinates you "
-            + "are inputting ((C)artesian / (P)olar): ");
+                  + "are inputting ((C)artesian / (P)olar): ");
         }
         else // Second and third arguments
         {
-          System.out.print("Enter the value of " 
-            + (coordType == 'C' 
-              ? (i == 1 ? "X " : "Y ")
-              : (i == 1 ? "Rho " : "Theta ")) 
-            + "using a decimal point(.): ");
+          System.out.print("Enter the value of "
+                  + (coordType == 'C'
+                  ? (i == 1 ? "X " : "Y ")
+                  : (i == 1 ? "Rho " : "Theta "))
+                  + "using a decimal point(.): ");
         }
 
-        // Get the user's input      
-       
+        // Get the user's input
+
         // Initialize the buffer before we read the input
         for(int k=0; k<1024; k++)
-        	buffer[k] = '\u0020';        
-             
+          buffer[k] = '\u0020';
+
         System.in.read(buffer);
         theInput = new String(buffer).trim();
-        
+
         // Verify the user's input
         try
         {
           if (i == 0) // First argument -- type of coordinates
           {
-            if (!((theInput.toUpperCase().charAt(0) == 'C') 
-              || (theInput.toUpperCase().charAt(0) == 'P')))
+            if (!((theInput.toUpperCase().charAt(0) == 'C')
+                    || (theInput.toUpperCase().charAt(0) == 'P')))
             {
               //Invalid input, reset flag so user is prompted again
               isOK = false;
@@ -149,15 +138,15 @@ public class PointCPTest
         }
         catch(Exception e)
         {
-        	System.out.println("Incorrect input");
-        	isOK = false;  //Reset flag as so not to end while loop
+          System.out.println("Incorrect input");
+          isOK = false;  //Reset flag as so not to end while loop
         }
       }
 
       //Reset flag so while loop will prompt for other arguments
       isOK = false;
     }
-    //Return a new PointCP object
-    return (new PointCP(coordType, a, b));
+    //Return a new design1.PointCP object
+    return (new PointCP4(coordType, a, b));
   }
 }
