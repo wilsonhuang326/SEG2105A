6ee5738c67829800bb2627134c4979dffe37e213
Lucas <lucaspitre@gmail.com>
2020-10-10 15:07:35 -0400

    Steps C, D, and E

--- lab3/app/src/main/java/com/example/simple_calculator/MainActivity.java
+++ lab3/app/src/main/java/com/example/simple_calculator/MainActivity.java
@@ -3,12 +3,39 @@ package com.example.simple_calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+    Button button0, button1, button2, button3, button4, button5, button6,
+            button7, button8, button9, buttonAdd, buttonSub, buttonMul,
+            buttonDiv, buttonDecimal, buttonC, buttonEql;
+    TextView outputTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button0 = (Button) findViewById(R.id.btn0);
+        button1 = (Button) findViewById(R.id.btn1);
+        button2 = (Button) findViewById(R.id.btn2);
+        button3 = (Button) findViewById(R.id.btn3);
+        button4 = (Button) findViewById(R.id.btn4);
+        button5 = (Button) findViewById(R.id.btn5);
+        button6 = (Button) findViewById(R.id.btn6);
+        button7 = (Button) findViewById(R.id.btn7);
+        button8 = (Button) findViewById(R.id.btn8);
+        button9 = (Button) findViewById(R.id.btn9);
+        buttonAdd = (Button) findViewById(R.id.btnAdd);
+        buttonSub = (Button) findViewById(R.id.btnSub);
+        buttonMul = (Button) findViewById(R.id.btnMul);
+        buttonDiv = (Button) findViewById(R.id.btnDiv);
+        buttonDecimal = (Button) findViewById(R.id.btnDecimal);
+        buttonC = (Button) findViewById(R.id.btnC);
+        buttonEql = (Button) findViewById(R.id.btnEql);
+        outputTxt = (TextView) findViewById(R.id.screen);
     }
 }
