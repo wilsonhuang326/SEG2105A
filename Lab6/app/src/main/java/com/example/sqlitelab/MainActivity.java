3a9c168983c6c21c7781e34ee67046fde28b96ad
wilson <wilsonhuang0326@gmail.com>
2020-11-07 20:14:54 -0500

    LAB6

---
+++ Lab6/app/src/main/java/com/example/sqlitelab/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.sqlitelab;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView idView;
+    EditText productBox;
+    EditText skuBox;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        idView = (TextView) findViewById(R.id.productID);
+        productBox = (EditText) findViewById(R.id.productName);
+        skuBox = (EditText) findViewById(R.id.productSku);
+    }
+
+
+
+    public void newProduct (View view) {
+
+        int sku = Integer.parseInt(skuBox.getText().toString());
+
+        Product product = new Product(productBox.getText().toString(), sku);
+
+        // TODO: add to database
+        MyDBHandler dbHandler=new MyDBHandler(this);
+        dbHandler.addProduct(product);
+
+        productBox.setText("");
+
+        skuBox.setText("");
+    }
+
+
+    public void lookupProduct (View view) {
+        MyDBHandler dbHandler=new MyDBHandler(this);
+        // TODO: get from Database
+        Product product = dbHandler.findProduct(productBox.getText().toString());
+
+        if (product != null) {
+            idView.setText(String.valueOf(product.getID()));
+            skuBox.setText(String.valueOf(product.getSku()));
+        } else {
+            idView.setText("No Match Found");
+        }
+    }
+
+
+    public void removeProduct (View view) {
+        MyDBHandler dbHandler=new MyDBHandler(this);
+
+        // TODO: remove from database
+        boolean result = dbHandler.deleteProduct(productBox.getText().toString());
+
+        if (result) {
+            idView.setText("Record Deleted");
+            productBox.setText("");
+            skuBox.setText("");
+        }
+        else
+            idView.setText("No Match Found");
+    }
+
+}
